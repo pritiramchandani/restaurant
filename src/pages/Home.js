fcc67a2a9af0a126931f7eaaad2856e016b60825
pritiramchandani <pritiramchandani124@gmail.com>
2023-11-19 20:36:45 +0530

    change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,21 @@ const Maindiv = styled.div`
 export const Home = () => {
   return (
     <><Navbar></Navbar>
-    <Maindiv className='bg-dark bg-image'>
-      <div className='container  hero-section text-white main-div'>
-        <div className='row vh-100  align-items-center'>
-          <div className='col-sm-6 pt-5'>
-            <h6 className='pt-5'>grab your smartphone, prepare your appetite, and embark on a captivating journey through these food quotes and captions that are sure to tantalize not only your followers' taste buds but also their imagination.</h6>
-            <button type="button" class="btn btn-outline-warning Reserve-button mt-5">Reserve a Table</button>
+      <Maindiv className='bg-dark bg-image'>
+        <div className='container  hero-section text-white main-div'>
+          <div className='row vh-100  align-items-center'>
+            <div className='col-sm-6 pt-5'>
+              <h6 className='pt-5 pe-4'>grab your smartphone, prepare your appetite, and embark 
+              on a captivating journey through these food quotes and captions that are 
+              sure to tantalize not only your followers' taste buds but also their imagination.</h6>
+              <div className='mt-5'>
+              <button type="button" class="btn btn-outline-warning Reserve-button mt-5">Reserve a Table</button>
+              </div>
+             
+            </div>
           </div>
         </div>
-      </div>
-    </Maindiv></>
-    
+      </Maindiv></>
+
   )
 }
